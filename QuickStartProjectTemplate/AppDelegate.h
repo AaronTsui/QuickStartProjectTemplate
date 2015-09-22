5ad9be3b125e26b8101d6241b6f551dd4e304a87
杯面君 <xu_jianye@qq.com>
2015-09-22 18:29:03 +0800

    cocoapods

---
+++ QuickStartProjectTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuickStartProjectTemplate
+//
+//  Created by 杯面君 on 15/9/22.
+//  Copyright © 2015年 ArronXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuickStartProjectTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickStartProjectTemplate
+//
+//  Created by 杯面君 on 15/9/22.
+//  Copyright © 2015年 ArronXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
